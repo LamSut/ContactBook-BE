911779d40c380375fc8a0d63e197756b7c2669f3
LamSut <lamsut0007@gmail.com>
2024-10-07 08:06:19 +0700

    Swagger res 404

--- src/routes/contacts.router.js
+++ src/routes/contacts.router.js
@@ -54,143 +54,152 @@ module.exports.setup = (app) => {
 
   router.get('/', contactsController.getContactsByFilter);
 
-/**
- * @swagger
- * /api/v1/contacts:
- *   post:
- *     summary: Create a new contact
- *     description: Create a new contact
- *     requestBody:
- *       required: true
- *       content:
- *         multipart/form-data:
- *           schema:
- *             $ref: '#/components/schemas/Contact'
- *     tags:
- *       - contacts
- *     responses:
- *       201:
- *         description: A new contact
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 status:
- *                   type: string
- *                   description: The response status
- *                   enum: [success]
- *                 data:
- *                   type: object
- *                   properties:
- *                     contact:
- *                       $ref: '#/components/schemas/Contact'
- */
+  /**
+   * @swagger
+   * /api/v1/contacts:
+   *   post:
+   *     summary: Create a new contact
+   *     description: Create a new contact
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         multipart/form-data:
+   *           schema:
+   *             $ref: '#/components/schemas/Contact'
+   *     tags:
+   *       - contacts
+   *     responses:
+   *       201:
+   *         description: A new contact
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               properties:
+   *                 status:
+   *                   type: string
+   *                   description: The response status
+   *                   enum: [success]
+   *                 data:
+   *                   type: object
+   *                   properties:
+   *                     contact:
+   *                       $ref: '#/components/schemas/Contact'
+   */
 
   router.post('/', avatarUpload, contactsController.createContact);
 
-/**
- * @swagger
- * /api/v1/contacts:
- *   delete:
- *     summary: Delete all contacts
- *     description: Delete all contacts
- *     tags:
- *       - contacts
- *     responses:
- *       200:
- *         description: All contacts deleted
- *         $ref: '#/components/responses/200NoData'
- */
+  /**
+   * @swagger
+   * /api/v1/contacts:
+   *   delete:
+   *     summary: Delete all contacts
+   *     description: Delete all contacts
+   *     tags:
+   *       - contacts
+   *     responses:
+   *       200:
+   *         description: All contacts deleted
+   *         $ref: '#/components/responses/200NoData'
+   */
 
   router.delete('/', contactsController.deleteAllContacts);
 
   router.all('/', methodNotAllowed);
 
 
-/**
- * @swagger
- * /api/v1/contacts/{id}:
- *   get:
- *     summary: Get contact by ID
- *     description: Get contact by ID
- *     parameters:
- *       - $ref: '#/components/parameters/contactIdParam'
- *     tags:
- *       - contacts
- *     responses:
- *       200:
- *         description: A contact
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 status:
- *                   type: string
- *                   description: The response status
- *                   enum: [success]
- *                 data:
- *                   type: object
- *                   properties:
- *                     contact:
- *                       $ref: '#/components/schemas/Contact'
- */
+  /**
+   * @swagger
+   * /api/v1/contacts/{id}:
+   *   get:
+   *     summary: Get contact by ID
+   *     description: Get contact by ID
+   *     parameters:
+   *       - $ref: '#/components/parameters/contactIdParam'
+   *     tags:
+   *       - contacts
+   *     responses:
+   *       200:
+   *         description: A contact
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               properties:
+   *                 status:
+   *                   type: string
+   *                   description: The response status
+   *                   enum: [success]
+   *                 data:
+   *                   type: object
+   *                   properties:
+   *                     contact:
+   *                       $ref: '#/components/schemas/Contact'
+   *       404:
+   *         description: Contact not found
+   *         $ref: '#/components/responses/404Contact'
+   */
 
   router.get('/:id', contactsController.getContact);
 
-/**
- * @swagger
- * /api/v1/contacts/{id}:
- *   put:
- *     summary: Update contact by ID
- *     description: Update contact by ID
- *     parameters:
- *       - $ref: '#/components/parameters/contactIdParam'
- *     requestBody:
- *       required: true
- *       content:
- *         multipart/form-data:
- *           schema:
- *             $ref: '#/components/schemas/Contact'
- *     tags:
- *       - contacts
- *     responses:
- *       200:
- *         description: An updated contact
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 status:
- *                   type: string
- *                   description: The response status
- *                   enum: [success]
- *                 data:
- *                   type: object
- *                   properties:
- *                     contact:
- *                       $ref: '#/components/schemas/Contact'
- */
+  /**
+   * @swagger
+   * /api/v1/contacts/{id}:
+   *   put:
+   *     summary: Update contact by ID
+   *     description: Update contact by ID
+   *     parameters:
+   *       - $ref: '#/components/parameters/contactIdParam'
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         multipart/form-data:
+   *           schema:
+   *             $ref: '#/components/schemas/Contact'
+   *     tags:
+   *       - contacts
+   *     responses:
+   *       200:
+   *         description: An updated contact
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               properties:
+   *                 status:
+   *                   type: string
+   *                   description: The response status
+   *                   enum: [success]
+   *                 data:
+   *                   type: object
+   *                   properties:
+   *                     contact:
+   *                       $ref: '#/components/schemas/Contact'
+   *       404:
+   *         description: Contact not found
+   *         $ref: '#/components/responses/404Contact'
+   */
 
   router.put('/:id', avatarUpload, contactsController.updateContact);
 
-/**
- * @swagger
- * /api/v1/contacts/{id}:
- *   delete:
- *     summary: Delete contact by ID
- *     description: Delete contact by ID
- *     parameters:
- *       - $ref: '#/components/parameters/contactIdParam'
- *     tags:
- *       - contacts
- *     responses:
- *       200:
- *         description: Contact deleted
- *         $ref: '#/components/responses/200NoData'
- */
+  /**
+   * @swagger
+   * /api/v1/contacts/{id}:
+   *   delete:
+   *     summary: Delete contact by ID
+   *     description: Delete contact by ID
+   *     parameters:
+   *       - $ref: '#/components/parameters/contactIdParam'
+   *     tags:
+   *       - contacts
+   *     responses:
+   *       200:
+   *         description: Contact deleted
+   *         $ref: '#/components/responses/200NoData'
+   *       404:
+   *         description: Contact not found
+   *         $ref: '#/components/responses/404Contact'
+   */
 
   router.delete('/:id', contactsController.deleteContact);
 
